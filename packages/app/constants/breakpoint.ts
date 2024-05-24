b2fd76931d5abac607e71046098cce6dff624ecb
sura.kh <sura.kh@rakuten.com>
2024-05-24 20:12:59 +0900

    test: fix test code

--- packages/app/constants/breakpoint.ts
+++ packages/app/constants/breakpoint.ts
@@ -1,14 +1,34 @@
-import { type DisplayBreakpointName, type DisplayOptions } from '@sui/app/types'
+import { type DisplayOptions } from '@sui/app/types'
 import breakpoints from '@sui/app/styles/modules/breakpoints.module.scss'
 import { z } from 'zod'
 
 export const scrollBarWidth: DisplayOptions['scrollBarWidth'] = 16 as const
 
-export const thresholds: DisplayOptions['thresholds'] = {
-  xs: z.coerce.number().parse(breakpoints.breakpointXs.replace('px', '')),
-  sm: z.coerce.number().parse(breakpoints.breakpointSm.replace('px', '')),
-  md: z.coerce.number().parse(breakpoints.breakpointMd.replace('px', '')),
-  lg: z.coerce.number().parse(breakpoints.breakpointLg.replace('px', '')),
-  xl: z.coerce.number().parse(breakpoints.breakpointXl.replace('px', '')),
-  xxl: z.coerce.number().parse(breakpoints.breakpointXxl.replace('px', '')),
-} satisfies Record<DisplayBreakpointName, number>
+const parsedThresholds = z
+  .object({
+    breakpointXs: z.preprocess((v) => (typeof v === 'string' ? Number(v.replace('px', '')) : undefined), z.number()),
+    breakpointSm: z.preprocess((v) => (typeof v === 'string' ? Number(v.replace('px', '')) : undefined), z.number()),
+    breakpointMd: z.preprocess((v) => (typeof v === 'string' ? Number(v.replace('px', '')) : undefined), z.number()),
+    breakpointLg: z.preprocess((v) => (typeof v === 'string' ? Number(v.replace('px', '')) : undefined), z.number()),
+    breakpointXl: z.preprocess((v) => (typeof v === 'string' ? Number(v.replace('px', '')) : undefined), z.number()),
+    breakpointXxl: z.preprocess((v) => (typeof v === 'string' ? Number(v.replace('px', '')) : undefined), z.number()),
+  })
+  .safeParse(breakpoints)
+
+export const thresholds: DisplayOptions['thresholds'] = parsedThresholds.success
+  ? {
+      xs: parsedThresholds.data.breakpointXs,
+      sm: parsedThresholds.data.breakpointSm,
+      md: parsedThresholds.data.breakpointMd,
+      lg: parsedThresholds.data.breakpointLg,
+      xl: parsedThresholds.data.breakpointXl,
+      xxl: parsedThresholds.data.breakpointXxl,
+    }
+  : {
+      xs: 640,
+      sm: 768,
+      md: 1024,
+      lg: 1280,
+      xl: 1536,
+      xxl: 1980,
+    }
