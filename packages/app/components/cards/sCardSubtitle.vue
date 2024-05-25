eb6dfeceb34a558c1c97b2653fed03be7862deb2
Sura Kh <sura0111@users.noreply.github.com>
2024-05-25 14:36:56 +0900

    fix: refactor sCardText, sCardSubtitle (#12)
    * fix: refactor sCardText, sCardSubtitle
    
    * ci: fix bug
    
    * ci: fix bug
    
    ---------
    
    Co-authored-by: sura.kh <sura.kh@rakuten.com>

--- packages/app/components/cards/sCardSubtitle.vue
+++ packages/app/components/cards/sCardSubtitle.vue
@@ -4,15 +4,14 @@
   </Component>
 </template>
 <script setup lang="ts">
-import { propsTag } from '@khsura/sui/props'
+import { type PropsTag } from '@khsura/sui/definitions'
 import { useTagService } from '@khsura/sui/services'
 
-const props = defineProps({
-  ...propsTag(),
-})
-
+// eslint-disable-next-line vue/no-unused-properties
+const props = defineProps<PropsTag>()
 const { tagName } = useTagService(props)
 </script>
+
 <style lang="scss">
 @import '@khsura/sui/styles/components/cards';
 
