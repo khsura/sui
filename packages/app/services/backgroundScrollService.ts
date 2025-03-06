6524d327e897a2ab049827f591070b6c405e6203
Sura Kh <sura.kh@tabsolution.jp>
2025-03-06 13:41:11 +0800

    fix: ssr issue

--- packages/app/services/backgroundScrollService.ts
+++ packages/app/services/backgroundScrollService.ts
@@ -1,10 +1,16 @@
+import { getWindow } from '@khsura/sui/lib'
 import { nextTick } from 'vue'
 import { z } from 'zod'
 
 export const useBackgroundScrollService = () => {
+  const window = getWindow()
+
   const enableBackgroundScroll = async () => {
-    const activeCount = document.querySelectorAll('.s_dialog__content--active, .s_navigationDrawer--active').length
-    const html = document.querySelector<HTMLElement>('html')
+    const activeCount = window?.document.querySelectorAll(
+      '.s_dialog__content--active, .s_navigationDrawer--active',
+    ).length
+
+    const html = window?.document.querySelector<HTMLElement>('html')
 
     if (activeCount === 1 && html) {
       const x = z.coerce.number().parse(html.style.getPropertyValue('--s-body-scroll-x').replace('px', ''))
@@ -16,16 +22,16 @@ export const useBackgroundScrollService = () => {
 
       await nextTick()
 
-      window.scrollTo({ left: -1 * x, top: -1 * y, behavior: 'instant' })
+      window?.scrollTo({ left: -1 * x, top: -1 * y, behavior: 'instant' })
     }
   }
 
   const disableBackgroundScroll = () => {
-    const html = document.querySelector<HTMLElement>('html')
+    const html = window?.document.querySelector<HTMLElement>('html')
 
-    if (html) {
-      html.style.setProperty('--s-body-scroll-x', `-${window.scrollX}px`)
-      html.style.setProperty('--s-body-scroll-y', `-${window.scrollY}px`)
+    if (html && window) {
+      html.style.setProperty('--s-body-scroll-x', `-${window?.scrollX}px`)
+      html.style.setProperty('--s-body-scroll-y', `-${window?.scrollY}px`)
       html.classList.add('s_overlay__scrollBlocked')
     }
   }
