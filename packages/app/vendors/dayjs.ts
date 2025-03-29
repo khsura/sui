9be23b63192ffcbdf1811479c4d32b4d6e783d0d
Sura Kh <sura.kh@tabsolution.jp>
2025-03-30 00:40:29 +0800

    fix: dayjs import bug

--- packages/app/vendors/dayjs.ts
+++ packages/app/vendors/dayjs.ts
@@ -1,4 +1,4 @@
-import dayjs, { extend, locale } from 'dayjs'
+import dayjs from 'dayjs'
 import customParseFormat from 'dayjs/plugin/customParseFormat.js'
 import isBetween from 'dayjs/plugin/isBetween.js'
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore.js'
@@ -7,6 +7,8 @@ import localizedFormat from 'dayjs/plugin/localizedFormat.js'
 import minMax from 'dayjs/plugin/minMax.js'
 import objectSupport from 'dayjs/plugin/objectSupport.js'
 import toObject from 'dayjs/plugin/toObject.js'
+
+const { extend, locale } = dayjs
 
 extend(localizedFormat)
 extend(localeData)
