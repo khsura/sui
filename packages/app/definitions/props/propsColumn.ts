03bafdb1c06e69ff6327f36ec5c70b92d601e58c
Sura Kh <sura0111@gmail.com>
2025-04-25 08:14:17 +0800

    fix: column

--- packages/app/definitions/props/propsColumn.ts
+++ packages/app/definitions/props/propsColumn.ts
@@ -1,6 +1,6 @@
 import type { PropsContent } from './core'
 
-export type ColumnNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 'auto'
+export type ColumnNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 'auto' | 'grow'
 
 export type PropsColumn = PropsContent & {
   cols?: ColumnNumber | `${ColumnNumber}` | null
