76e69aeb813d4d2d5fe985995953ceb88b2ddf06
Sura Kh <sura0111@gmail.com>
2026-01-13 15:35:54 +0800

    fix: table

--- packages/app/services/table/tableService.ts
+++ packages/app/services/table/tableService.ts
@@ -1,5 +1,6 @@
-import { computed, ref, type Ref, type EmitFn, type ShallowRef, useId } from 'vue'
+import { computed, ref, type Ref, type EmitFn, type ShallowRef } from 'vue'
 import { z } from 'zod'
+import { v4 as uuidv4 } from 'uuid'
 import type STableHeadCell from '@/app/components/table/sTableHeadCell.vue'
 import { getTableRowClass } from '@/app/helpers'
 import { getCleanSetObject, getNumericCssAttribute } from '@/app/lib'
@@ -16,7 +17,7 @@ export const useTableService = <T extends TableItem = TableItem>(
 ) => {
   const isMounted = ref(false)
   const sortOrders = ref<Record<string, KTableSortOrder | undefined>>({})
-  const tableId = ref(useId())
+  const tableId = ref(uuidv4())
   const isLeftStickActive = ref(false)
   const pageIndex = ref(0)
 
@@ -399,6 +400,12 @@ export const useTableService = <T extends TableItem = TableItem>(
 
     const cell = getCellInfo(header)
 
+    if (cell?.isSticked) {
+      console.log('cell', cell)
+      console.log('store.width', store.width)
+      console.log('tableId.value', tableId.value)
+    }
+
     if (!cell?.isSticked || !store.width || !tableId.value) {
       return undefined
     }
@@ -431,7 +438,7 @@ export const useTableService = <T extends TableItem = TableItem>(
   }
 
   const updateTable = () => {
-    tableId.value = useId()
+    tableId.value = uuidv4()
   }
 
   return {
