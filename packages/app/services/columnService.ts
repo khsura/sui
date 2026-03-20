6ecc64ac8eb240d0cd80a87da9dfd76acc95f93f
Sura Kh <sura0111@gmail.com>
2026-03-20 18:05:49 +0800

    revert: column service

--- packages/app/services/columnService.ts
+++ packages/app/services/columnService.ts
@@ -1,20 +1,23 @@
 import { computed } from 'vue'
+import { appPrefix } from '@/app/constants'
 import { type PropsColumn } from '@/app/definitions'
 import { getCleanSetObject } from '@/app/lib'
 
-export const useColumnService = (props: PropsColumn) => {
+export const useColumnService = (props: PropsColumn, options?: { classPrefix?: string }) => {
+  const prefix = options?.classPrefix ? `_${options.classPrefix}` : '_column'
+
   const getIsSet = (value?: number | string | string | null) => {
     return value !== null && value !== undefined
   }
 
   const classListColumn = computed(() => {
     return getCleanSetObject({
-      [`s_column__${props.cols}`]: getIsSet(props.cols),
-      [`s_column--sm__${props.sm}`]: getIsSet(props.sm),
-      [`s_column--md__${props.md}`]: getIsSet(props.md),
-      [`s_column--lg__${props.lg}`]: getIsSet(props.lg),
-      [`s_column--order__${props.order}`]: getIsSet(props.order),
-      [`s_column--alignSelf__${props.alignSelf}`]: getIsSet(props.alignSelf),
+      [`${appPrefix}${prefix}__${props.cols}`]: getIsSet(props.cols),
+      [`${appPrefix}${prefix}--sm__${props.sm}`]: getIsSet(props.sm),
+      [`${appPrefix}${prefix}--md__${props.md}`]: getIsSet(props.md),
+      [`${appPrefix}${prefix}--lg__${props.lg}`]: getIsSet(props.lg),
+      [`${appPrefix}${prefix}--order__${props.order}`]: getIsSet(props.order),
+      [`${appPrefix}${prefix}--alignSelf__${props.alignSelf}`]: getIsSet(props.alignSelf),
     })
   })
 
