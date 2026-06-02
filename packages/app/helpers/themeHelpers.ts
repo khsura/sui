0ab9645cd9bbd22cc98d96ad56dfbad89fb1eda4
Sura Kh <sura0111@gmail.com>
2026-06-02 11:01:52 +0800

    refactor(theme): deprecate getBrowserTheme in favor of useTheme

--- packages/app/helpers/themeHelpers.ts
+++ packages/app/helpers/themeHelpers.ts
@@ -2,6 +2,11 @@ import { AppTheme } from '@/app/constants'
 import { getWindow } from '@/app/lib'
 import type { AppThemeType } from '@/app/types'
 
+/**
+ * @deprecated Use `useTheme()` instead. This performs a one-shot, non-reactive
+ * read of the OS preference. `useTheme().theme` is reactive and persisted, and
+ * `setTheme('auto')` follows the OS live.
+ */
 export const getBrowserTheme = (): AppThemeType => {
   const window = getWindow()
 
